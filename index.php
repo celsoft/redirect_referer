4b66ec82583d583e4cb7ce8bdc3a7eb80fa08871
Celsoft <maksim_blg@mail.ru>
2020-11-19 16:40:56 +0300

    new

--- index.php
+++ index.php
@@ -20,8 +20,7 @@ if ($isSearchBot and stripos($userIpASN, 'Google Fiber') !== false) {
     $isSearchBot = false;
 }
 
-$oldDomain = 'apparatov.net';
-$redirectDomain = 'apparatov.azurewebsites.net';
+$redirectDomain = 'igrovye-avtomaty.azurewebsites.net';
 
 $yandexPos = stripos($user_referer, 'yandex');
 $sitePos = stripos($user_referer, $redirectDomain);
@@ -32,26 +31,3 @@ if ( !$user_referer ){
 
 header('Location: https://'.$redirectDomain.$serverRequestUri, true, 301);
 exit;
-
-function curlProxy($mirror)
-{
-    global $oldDomain, $redirectDomain, $user_agent;
-    $url = "https://{$mirror}{$_SERVER['REQUEST_URI']}";
-    // create a new cURL resource
-    $ch = curl_init();
-    // set URL and other appropriate options
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
-    $result = curl_exec($ch);
-    $result = str_replace($oldDomain, $redirectDomain, $result);
-    $info = curl_getinfo($ch);
-    $contentType = $info['content_type'];
-    @header("Content-Type: $contentType");
-    // close cURL resource, and free up system resources
-    curl_close($ch);
-    return $result;
-}
